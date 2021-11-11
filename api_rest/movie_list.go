69acd9934042f216f547882fd67d4eb109c670e2
cesarvargas1990 <ceavarga@bancolombia.com.co>
2021-11-10 23:08:22 -0500

    ajuste para ordenar

--- api_rest/movie_list.go
+++ api_rest/movie_list.go
@@ -9,7 +9,7 @@ import (
 
 func MovieList(w http.ResponseWriter, r *http.Request) {
 	var results []Movie
-	err := collection.Find(nil).All(&results)
+	err := collection.Find(nil).Sort("-_id").All(&results)
 	if err != nil {
 		log.Fatal(err)
 	} else {
