6aa0c951e2afa66bcf3e7c8df810c79d21cb27aa
cesarvargas1990 <ceavarga@bancolombia.com.co>
2021-11-10 23:20:41 -0500

    metodo para mostrar un elemento dado un id

--- api_rest/movie_show.go
+++ api_rest/movie_show.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
-	"fmt"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func MovieShow(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	movie_id := params["id"]
-	fmt.Fprintf(w, "Has cargado la pelicula numero %s",movie_id)
+	if !bson.IsObjectIdHex(movie_id) {
+		w.WriteHeader(404)
+		return
+	}
+	oid := bson.ObjectIdHex(movie_id)
+	results := Movie{}
+	err := collection.FindId(oid).One(&results)
+	if err!= nil {
+		w.WriteHeader(404)
+		return
+	} 
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(results)
 }
